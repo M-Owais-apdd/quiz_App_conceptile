a1643fb0b202dda4c0e2da611e4251c6488b8284
Mohd Owais <133536242+M-Owais-apdd@users.noreply.github.com>
2024-12-14 12:36:25 +0530

    Update urls.py

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    #path('', views.index,name='index'),
+    path('', views.index,name='index'),
     path('start-quiz/', views.start_quiz, name='start_quiz'),
     path('get-question/', views.get_question, name='get_question'),
     path('submit-answer/', views.submit_answer, name='submit_answer'),
